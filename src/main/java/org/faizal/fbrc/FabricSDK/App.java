a5d4eb68f5a7599ef01f62c2044f799b306f76fc
faizal <smfaizalkhan@gmail.com>
2017-07-19 17:33:53 +0400

    JavaHyperledger

---
+++ src/main/java/org/faizal/fbrc/FabricSDK/App.java
@@ -0,0 +1,13 @@
+package org.faizal.fbrc.FabricSDK;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
